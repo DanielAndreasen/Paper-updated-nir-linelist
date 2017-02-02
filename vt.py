c21640e3c9ab5de380bf0780c39d52c772a56d1a
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-02-02 16:49:02 +0000

    Add Simbad data for Arcturus and 10 Leo

---
+++ vt.py
@@ -0,0 +1,7 @@
+def _getMic(teff, logg, feh):
+    if logg >= 3.95:  # Dwarfs Tsantaki 2013
+        mic = 6.932 * teff * (10**(-4)) - 0.348 * logg - 1.437
+        return round(mic, 2)
+    else:  # Giants Adibekyan 2015
+        mic = 2.72 - (0.457 * logg) + (0.072 * feh)
+        return round(mic, 2)
